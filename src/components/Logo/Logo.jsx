d5f343930e627c13e8dc2f35b5eddf2fe464e8ed
BakelD <bakeld85@gmail.com>
2025-06-09 19:24:32 +0200

    Add href attribute to Logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -2,7 +2,7 @@ import css from './Logo.module.css';
 
 const Logo = () => {
   return (
-    <a className={css.logo}>
+    <a className={css.logo} href="./index.html">
       <span className={css.accent}>Web</span>Studio
     </a>
   );
