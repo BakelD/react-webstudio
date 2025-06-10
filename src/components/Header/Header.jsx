da5b071d739476acbbbe6287a46a9b55cb1fb5a9
BakelD <bakeld85@gmail.com>
2025-06-10 11:16:10 +0200

    Change Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,23 @@
+import ContactInfo from '../ContactInfo/ContactInfo';
+import Container from '../Container/Container';
+import NavMenu from '../NavMenu/NavMenu';
+import Logo from '../Logo/Logo';
+
+import menuItems from '../../data/menu.json';
+import contacts from '../../data/contacts.json';
+
 import css from './Header.module.css';
 
-const Header = ({ children }) => {
-  return <header className={css.header}>{children}</header>;
+const Header = () => {
+  return (
+    <header className={css.header}>
+      <Container isFlex>
+        <Logo />
+        <NavMenu menuItems={menuItems} />
+        <ContactInfo contacts={contacts} />
+      </Container>
+    </header>
+  );
 };
 
 export default Header;
