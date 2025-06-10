eae3757d9ce841727392facac90b939d59ef9ce4
BakelD <bakeld85@gmail.com>
2025-06-10 11:02:27 +0200

    Add key to ContactInfo items

--- src/components/ContactInfo/ContactInfo.jsx
+++ src/components/ContactInfo/ContactInfo.jsx
@@ -3,10 +3,10 @@ import css from './ContactInfo.module.css';
 const ContactInfo = ({ contacts }) => {
   return (
     <ul className={css.list}>
-      {contacts.map(item => (
-        <li>
-          <a className={css.link} href="">
-            {item.info}
+      {contacts.map(({ id, info, url }) => (
+        <li key={id}>
+          <a className={css.link} href={url}>
+            {info}
           </a>
         </li>
       ))}
